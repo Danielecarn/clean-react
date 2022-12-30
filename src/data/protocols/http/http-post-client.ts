a48440f7ceaa4a2937224eadb78e2a65486038b4
Daniele Carnaúba <dannygoncalves41@gmail.com>
2022-12-30 02:34:14 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -4,6 +4,6 @@ export type HttpPostParams<T> = {
   url: string
   body?: T
 }
-export interface HttpPostCliente<T, R> {
+export interface HttpPostClient<T, R> {
   post (params: HttpPostParams<T>): Promise<HttpResponse<R>>
 }
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,15 +1,15 @@
-import { HttpPostCliente, HttpStatusCode } from '@/data/protocols/http'
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
 import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
 import { AuthenticationParams, Authentication } from '@/domain/usecases'
 export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
-    private readonly httpPostCliente: HttpPostCliente<AuthenticationParams, AccountModel>
+    private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
   async auth (params: AuthenticationParams): Promise<AccountModel> {
-    const httpResponse = await this.httpPostCliente.post({
+    const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
