b559c28e8f4f73328bcb9e6b50b9e010a35f7d8f
Daniele Carnaúba <dannygoncalves41@gmail.com>
2022-12-30 00:07:44 -0300

    refactor: add generics params to HttpPostClient

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,9 +1,9 @@
 import { HttpResponse } from './http-response'
 
-export type HttpPostParams = {
+export type HttpPostParams<T> = {
   url: string
-  body?: object
+  body?: T
 }
-export interface HttpPostCliente {
-  post (params: HttpPostParams): Promise<HttpResponse>
+export interface HttpPostCliente<T, R> {
+  post (params: HttpPostParams<T>): Promise<HttpResponse<R>>
 }
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -2,12 +2,13 @@ import { HttpPostCliente } from '@/data/protocols/http/http-post-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { AccountModel } from '@/domain/models/account-model'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 
 export class RemoteAuthentication {
   constructor (
     private readonly url: string,
-    private readonly httpPostCliente: HttpPostCliente
+    private readonly httpPostCliente: HttpPostCliente<AuthenticationParams, AccountModel>
   ) {}
 
   async auth (params: AuthenticationParams): Promise<void> {
